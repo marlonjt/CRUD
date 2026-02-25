2a57f2a4a64a415bd4d82ac1442b75f8097ae6a6
{marlonjt} <{marlonj3@outlook.es}>
2026-02-24 20:10:19 -0500

    Usa safeLoad para compatibilidad con Railway sin .env

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 try {
     $pdo = new PDO(
